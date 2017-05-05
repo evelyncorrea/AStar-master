1a66caaeac75d855f276adf78a500f20c50893de
Evelyn Correa <evelynnave2015@gmail.com>
2017-05-05 10:24:46 -0300

    Pathfinding
    1.0 version
    Evelyn, Henrique e João Pedro Santos
    3003

---
+++ Assets/Scripts/PathFinder.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+
+public static class PathFinder
+{
+	public static List<Grid.Position> FindPath( Tile[,] tiles, Grid.Position fromPosition, Grid.Position toPosition)
+	{
+		var path = new List<Grid.Position>();
+		path.Add( fromPosition );
+        Grid.Position posi = fromPosition;
+
+        while(posi.x != toPosition.x || posi.y != toPosition.y)
+        {
+            if(posi.x < toPosition.x)
+            { posi.x++; }
+            else if (posi.x>toPosition.x) { posi.x--; }
+
+            if (posi.y < toPosition.y)
+            { posi.y++; }
+            else if (posi.y> toPosition.y) { posi.y--; }
+           
+            path.Add(posi);
+        }
+        
+        path.Add(toPosition);
+        return path;
+	}
+}
